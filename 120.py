35071692d8682ebff75981d91b201a9f724771a6
Roberto Augusto Speglic Mendes Mesquita <60950148+Speglic@users.noreply.github.com>
2020-03-03 22:04:30 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,10 @@
+#autores:  1DS3 
+#@Roberto Augusto Speglic Mendes Mesquita 
+#feito em 03/03/2020
+#Instituição: Etec
+x = 0 #declaraçao de variavel valor
+valor = 2 #declaraçao de variavel valor
+while (x <= 21):
+	valor = valor * valor
+	x = x + 1 #Laço de Repetição
+	print (valor) #Exibir variavel valor 
